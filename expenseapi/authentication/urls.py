d45d2c8db94669b3c02a14ddeef20e7a2d087aa3
Dorothy2020 <wangarraakoth@gmail.com>
2023-11-24 07:15:35 +0300

    chore:remove imports

--- expenseapi/authentication/urls.py
+++ expenseapi/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
 from .views import RegisterView, VerifyEmail
+from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg.views import openapi
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
