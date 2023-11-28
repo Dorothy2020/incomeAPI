61c2c23803809c9cb5215a2db450f0a615779c29
Dorothy2020 <wangarraakoth@gmail.com>
2023-11-28 06:29:40 +0300

    feat:add swagger UI into the application

--- expenseapi/authentication/urls.py
+++ expenseapi/authentication/urls.py
@@ -4,7 +4,22 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg.views import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="INCOME EXPENSE API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="contact@expenses.local"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
-    path('email-verify/', VerifyEmail.as_view(), name="email-verify")
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
